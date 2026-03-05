18d4880cdbd387828dabb83090a8411e3ccf0ead
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-05 13:17:56 +0000

    Seller Register & Login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const adminModel = require('../models/adminModel')
+const sellerModel = require('../models/sellerModel')
+const sellerCustomerModel  = require('../models/chat/sellerCustomerModel')
 const { responseReturn } = require('../utiles/response')
 const bcrpty = require('bcrypt')
 const { createToken } = require('../utiles/tokenCreate')
@@ -36,6 +38,42 @@ class authControllers{
 
     // End Method 
 
+
+    seller_register = async(req, res) => {
+         const {email,name,password} = req.body
+         try {
+            const getUser = await sellerModel.findOne({email})
+            if (getUser) {
+                responseReturn(res,404,{error: 'Email Already Exit'})
+            }else{
+                const seller = await sellerModel.create({
+                    name,
+                    email,
+                    password: await bcrpty.hash(password, 10),
+                    method : 'menualy',
+                    shopInfo: {}
+                })
+                 await sellerCustomerModel.create({
+                     myId: seller.id
+               })
+
+               const token = await createToken({ id : seller.id, role: seller.role })
+               res.cookie('accessToken',token, {
+                expires : new Date(Date.now() + 7*24*60*60*1000 )
+               })
+
+               responseReturn(res,201,{token,message: 'Register Success'})
+            }
+         } catch (error) {
+            responseReturn(res,500,{error: 'Internal Server Error'})
+         }
+    }
+
+      // End Method 
+
+
+
+
     getUser = async (req, res) => {
         const {id, role} = req;
 
@@ -52,8 +90,10 @@ class authControllers{
         }
 
 
-    } // End getUser Method 
+    } 
     
+    // End getUser Method 
+
 }
 
 module.exports = new authControllers()
