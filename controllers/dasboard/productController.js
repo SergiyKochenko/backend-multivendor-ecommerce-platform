46c387544d7809c6b09fe4e34dd81b9f7306bc97
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-06 17:49:06 +0000

    Continue on - Setup Category Post & Get

---
+++ controllers/dasboard/productController.js
@@ -0,0 +1,50 @@
+const formidable = require("formidable")
+const { responseReturn } = require("../../utiles/response")
+const cloudinary = require('cloudinary').v2
+const productModel = require('../../models/productModel')
+
+class productController{
+
+    add_product = async(req,res) => {
+        const form = formidable({ multiples: true })
+
+        form.parse(req, async(err, field, files) => {
+            let {name, category, description, stock, price, discount, shopName, brand} = field;
+            const {images} = files;
+            name = name.trim()
+            const slug = name.split(' ').join('-')
+
+            cloudinary.config({
+                cloud_name: process.env.cloud_name,
+                api_key: process.env.api_key,
+                api_secret: process.env.api_secret,
+                secure: true
+            })
+
+            try {
+                let allImageUrl = [];
+                for (let i = 0; i < images.length; i++) {
+                    const result = await cloudinary.uploader.upload(images[i].filepath, {folder: 'products'});
+                    allImageUrl = [...allImageUrl, result.url] 
+                }
+
+                await productModel.create({
+                    
+                })
+
+                
+            } catch (error) {
+                
+            }
+
+
+        })
+
+    }
+
+    /// end method 
+
+
+}
+
+module.exports = new productController()
