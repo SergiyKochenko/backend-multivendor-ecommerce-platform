9cf4a4b2afd27adcfff671c93fe6e3b37241a600
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2026-03-12 20:09:26 +0000

    Product Query And Show

---
+++ utiles/queryProducts.js
@@ -0,0 +1,78 @@
+class queryProducts {
+  products = [];
+  query = {};
+  constructor(products, query) {
+    this.products = products;
+    this.query = query;
+  }
+
+  categoryQuery = () => {
+    this.products = this.query.category
+      ? this.products.filter((c) => c.category === this.query.category)
+      : this.products;
+    return this;
+  };
+
+  ratingQuery = () => {
+    this.products = this.query.rating
+      ? this.products.filter(
+          (c) =>
+            parseInt(this.query.rating) <= c.rating &&
+            c.rating < parseInt(this.query.rating) + 1,
+        )
+      : this.products;
+    return this;
+  };
+
+  priceQuery = () => {
+    const { lowPrice, highPrice } = this.query;
+    if (lowPrice === undefined && highPrice === undefined) return this;
+
+    const minPrice = lowPrice !== undefined ? Number(lowPrice) : 0;
+    const maxPrice =
+      highPrice !== undefined ? Number(highPrice) : Number.MAX_SAFE_INTEGER;
+
+    this.products = this.products.filter(
+      (p) => p.price >= minPrice && p.price <= maxPrice,
+    );
+    return this;
+  };
+  sortByPrice = () => {
+    if (this.query.sortPrice) {
+      if (this.query.sortPrice === "low-to-high") {
+        this.products = this.products.sort(function (a, b) {
+          return a.price - b.price;
+        });
+      } else {
+        this.products = this.products.sort(function (a, b) {
+          return b.price - a.price;
+        });
+      }
+    }
+    return this;
+  };
+
+  skip = () => {
+    const pageNumber = parseInt(this.query.pageNumber || 1);
+    const parPage = parseInt(this.query.parPage) || 12;
+    const skipPage = (pageNumber - 1) * parPage;
+    this.products = this.products.slice(skipPage);
+    return this;
+  };
+
+  limit = () => {
+    const parPage = parseInt(this.query.parPage) || 12;
+    this.products = this.products.slice(0, parPage);
+    return this;
+  };
+
+  getProducts = () => {
+    return this.products;
+  };
+
+  countProducts = () => {
+    return this.products.length;
+  };
+}
+
+module.exports = queryProducts;
